c720f9f420f23f1953e5a5be4abc256f8fd55ae1
Liliya Wu <lilwu01@a015840.shanghai.arm.com>
2025-09-09 00:42:39 +0800

    Update supported model

--- model_zoo/Ppocr_layout_infer_int8/layout_postprocess/pdnpy2bin.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-np.load("./pdlite_npy/output_tensor_1x37x100x76.npy").tofile("output/output_0.bin")
-np.load("./pdlite_npy/output_tensor_1x37x50x38.npy").tofile("output/output_1.bin")
-np.load("./pdlite_npy/output_tensor_1x37x25x19.npy").tofile("output/output_2.bin")
-np.load("./pdlite_npy/output_tensor_1x37x13x10.npy").tofile("output/output_3.bin")
